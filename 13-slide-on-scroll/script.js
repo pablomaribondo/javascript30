0c04e8f3c15f71116a1ac38b4e5afc05ce72edbf
Pablo Maribondo <pablomgvieira@gmail.com>
2022-02-09 20:50:19 -0300

    day13-slide-on-scroll

---
+++ 13-slide-on-scroll/script.js
@@ -0,0 +1,35 @@
+function debounce(func, wait = 20, immediate = true) {
+  let timeout;
+  return function () {
+    const context = this,
+      args = arguments;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+const sliderImages = document.querySelectorAll(".slide-in");
+
+const checkSlide = event => {
+  sliderImages.forEach(sliderImage => {
+    const slideInAt =
+      window.scrollY + window.innerHeight - sliderImage.height / 2;
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add("active");
+    } else {
+      sliderImage.classList.remove("active");
+    }
+  });
+};
+
+window.addEventListener("scroll", debounce(checkSlide));
